49cd72ec08cb856cd6d5f61d4616164cbdbbe17c
iko <ilyakooo0@gmail.com>
2021-02-24 20:54:40 +0300

    Improved things

--- default.nix
+++ default.nix
@@ -1,16 +1,30 @@
 { sources ? import nix/sources.nix { inherit system; }
 , nixpkgs ? import sources.nixpkgs { inherit system; }
 , system ? builtins.currentSystem
+, ghc ? "ghc8103"
 }:
 let
-  proto3-suite' = import sources.proto3-suite { };
-  proto3-suite =
-    if system == "x86_64-linux" then proto3-suite'.proto3-suite-linux
-    else if system == "x86_64-darwin" then proto3-suite'.proto3-suite-darwin
-    else abort "proto3-suite only supports x86_64-linux and x86_64-darwin at the moment :(";
+  unbreak = x: x.overrideDerivation (_: {
+    broken = false;
+  });
+  overrides = (hself: hsuper: {
+    proto3-suite = nixpkgs.haskell.lib.dontCheck (nixpkgs.haskell.lib.doJailbreak (unbreak hsuper.proto3-suite));
+    proto3-wire =
+      hsuper.callHackageDirect
+        {
+          pkg = "proto3-wire";
+          ver = "1.2.0";
+          sha256 = "1jqz6zsli5zvlissy7mkgyrzkapjvgijx7kjva4fxjwdyd0hqix7";
+        } { };
+    haskell-src = nixpkgs.haskell.lib.doJailbreak (unbreak hsuper.haskell-src);
+    parameterized = nixpkgs.haskell.lib.dontCheck (nixpkgs.haskell.lib.doJailbreak (unbreak hsuper.parameterized));
+  });
+  haskell = nixpkgs.haskell.packages.${ghc}.override {
+    inherit overrides;
+  };
   sawtooth-haskell-protos-src = nixpkgs.stdenv.mkDerivation {
     name = "sawtooth-haskell-protos-src";
-    buildInputs = [ proto3-suite ];
+    buildInputs = [ haskell.proto3-suite ];
     src = sources.sawtooth-core + "/protos";
     buildPhase = ''
       for f in *.proto
@@ -35,59 +49,13 @@ let
   };
   sawtooth-haskell-protos =
     nixpkgs.haskellPackages.callCabal2nix "sawtooth-haskell-protos" sawtooth-haskell-protos-src { };
-  sawtooth-haskell-protos-overlay = hself: hsuper: {
+  sawtooth-haskell-protos-overlay = nixpkgs.lib.composeExtensions overrides (hself: hsuper: {
     sawtooth-haskell-protos = hsuper.callCabal2nix "sawtooth-haskell-protos" sawtooth-haskell-protos-src { };
-    haskell-src = nixpkgs.haskell.lib.doJailbreak (hsuper.callHackageDirect
-      {
-        pkg = "haskell-src";
-        ver = "1.0.3.1";
-        sha256 = "11s2qnnhchcbi6szvcglv4xxz3l6zw9w3pziycpwrigjvpigymd2";
-      } { }
-    );
-    insert-ordered-containers = hsuper.callHackageDirect
-      {
-        pkg = "insert-ordered-containers";
-        ver = "0.2.3.1";
-        sha256 = "0zxxjwzcsyc7a572f584w83aqygdc3q05pfarghj2v437nsnap29";
-      } { };
-    swagger2 = hsuper.callHackageDirect
-      {
-        pkg = "swagger2";
-        ver = "2.6";
-        sha256 = "0x0s34q9bmrik0vmzpc08r4jq5kbpb8x7h19ixhaakiafpjfm59l";
-      } { };
-    proto3-suite = nixpkgs.haskell.lib.dontCheck (hsuper.callHackageDirect
-      {
-        pkg = "proto3-suite";
-        ver = "0.4.0.2";
-        sha256 = "11bppmb524q6qvgyddi9s7pf1n1zs4ypqkqs2qb6i8nsgmxndd1l";
-      } { }
-    );
-    proto3-wire = hsuper.callHackageDirect
-      {
-        pkg = "proto3-wire";
-        ver = "1.2.0";
-        sha256 = "1jqz6zsli5zvlissy7mkgyrzkapjvgijx7kjva4fxjwdyd0hqix7";
-      } { };
-    http-media = nixpkgs.haskell.lib.doJailbreak (hsuper.callHackageDirect
-      {
-        pkg = "http-media";
-        ver = "0.8.0.0";
-        sha256 = "080xkljq1iq0i8wagg8kbzbp523p2awa98wpn9i4ph1dq8y8346y";
-      } { }
-    );
-    parameterized = nixpkgs.haskell.lib.dontCheck (hsuper.callHackageDirect
-      {
-        pkg = "parameterized";
-        ver = "0.5.0.0";
-        sha256 = "sha256-FB3E0ChUG1x6uYE8fL7hcncS5Xg0mqNOxc7KlLhEdB0=";
-      } { }
-    );
-  };
+  });
 in
 {
   inherit sawtooth-haskell-protos-src sawtooth-haskell-protos-overlay;
-  build = (nixpkgs.haskell.packages.ghc882.override {
+  build = (nixpkgs.haskell.packages.${ghc}.override {
     overrides = sawtooth-haskell-protos-overlay;
   }).sawtooth-haskell-protos;
 }
